2f7a1f637ec91bc3155a0d999f9d2b7a257e58d9
Damlima <dlima@fi.uba.ar>
2023-11-16 10:10:57 -0300

    Pruebo patron de diseño para nivel

--- src/main/test/edu/fiuba/algo3/entrega_1/MessageTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/MessageTest.java
@@ -1,15 +1,16 @@
 package edu.fiuba.algo3.entrega_1;
 
-import edu.fiuba.algo3.modelo.Message;
+import edu.fiuba.algo3.modelo.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MessageTest {
     @Test
-    public void messageGreeting() {
-        Message message = new Message("Hola Mundo!", "Hello world!");
+    public void jugadorEmpiezaConLaEnergiaYEquipamientoCorrespondiente() {
+        NivelFactory nivel = new NivelFactory();
+        Nivel novato = nivel.obtenerInstancia("NOVATO");
 
-        assertEquals("Hello world!", message.greet("us"));
+
     }
 
     @Test

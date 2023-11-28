7a4456ea868a1780dbc1971d3dda44dded87707b
Damlima <dlima@fi.uba.ar>
2023-11-28 11:21:07 -0300

    error en segundaEntrada

--- src/main/test/edu/fiuba/algo3/entrega_2/SegundaEntregaTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_2/SegundaEntregaTest.java
@@ -1,20 +1,16 @@
 package edu.fiuba.algo3.entrega_2;
 
-import edu.fiuba.algo3.modelo.DataClassCelda;
-import edu.fiuba.algo3.modelo.DataClassTablero;
-import edu.fiuba.algo3.modelo.JsonReader;
+import edu.fiuba.algo3.modelo.AlgoRoma;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class SegundaEntregaTest {
 
     @Test
-    void test16lecturaYConversionCorrectaAlModeloDeDominioDelProblema() throws IOException {
-        //Arrange
-        DataClassTablero dataTablero = null;
-        assertNull(dataTablero);
+    public void leerJson() throws IOException {
+        //var lista = AlgoRoma.obtenerListaCaminoDesdeJson("mapa.json");
+        //var tamanio = AlgoRoma.obtenerTamanio("mapa.json");
+
     }
 }

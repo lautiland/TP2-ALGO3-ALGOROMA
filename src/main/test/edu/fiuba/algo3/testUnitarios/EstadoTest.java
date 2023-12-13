dbc7de5d2a064fd281f769017d9d99d75737f4df
Matias Vallejos <matiavallejo@gmail.com>
2023-12-13 19:52:22 -0300

    Arreglado bug se movia sin energia

--- src/main/test/edu/fiuba/algo3/testUnitarios/EstadoTest.java
+++ src/main/test/edu/fiuba/algo3/testUnitarios/EstadoTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -70,5 +71,43 @@ public class EstadoTest {
         gladiador.jugarTurno(camino);
 
         assertTrue(camino.estaEl(gladiador, 2));
+    }
+
+    @Test
+    public void test04NoSeMueveSinEnergia() {
+        Dado dadoMock = mock(Dado.class, "Dado");
+        when(dadoMock.tirar()).thenReturn(1);
+        Gladiador gladiador = new Gladiador("Augustus", dadoMock);
+        Camino camino = new Camino(List.of(gladiador));
+        Interactuable sinInteractuable = InteractuableFactory.crearInteractuable("Ninguno");
+        Interactuable Fiera = InteractuableFactory.crearInteractuable("Fiera");
+        Interactuable bacanal = InteractuableFactory.crearInteractuable("Bacanal");
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+        camino.agregarCasilla(new Casilla(sinInteractuable, Fiera));
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+        camino.agregarCasilla(new Casilla(sinInteractuable, bacanal));
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+        camino.agregarCasilla(new Casilla(sinInteractuable, sinInteractuable));
+
+        gladiador.jugarTurno(camino);// Se encuentra a la fiera y se queda sin energia
+        gladiador.jugarTurno(camino); // Recupera energia, 5 energia
+        when(dadoMock.tirar()).thenReturn(6);
+        gladiador.jugarTurno(camino); // Se encuentra al bacanal, 5-30 = -25 energia
+        assertEquals(-25, gladiador.obtenerPuntosEnergia());
+        assertEquals(7, camino.obtenerPosicionDe(gladiador));
+
+        when(dadoMock.tirar()).thenReturn(1);
+        gladiador.jugarTurno(camino); // No se mueve, -20Puntos
+        assertEquals(-20, gladiador.obtenerPuntosEnergia());
+        assertEquals(7, camino.obtenerPosicionDe(gladiador));
+
+        when(dadoMock.tirar()).thenReturn(1);
+        gladiador.jugarTurno(camino); // No se mueve -15Puntos
+        assertEquals(-15, gladiador.obtenerPuntosEnergia());
+        assertEquals(7, camino.obtenerPosicionDe(gladiador));
     }
 }
